048b1cf386024e627a158b1fc055dd79ac44e05b
Arc <atandarokib@gmail.com>
2023-07-24 01:39:21 +0100

    up

--- src/Des.jsx
+++ src/Des.jsx
@@ -5,7 +5,7 @@ function Des() {
     <section className="py-14 w-full">
       <div className="container flex gap-7 justify-center items-center flex-col">
         <h1 className="text-5xl md:text-6xl uppercase text-center max-w-xl">
-          win <span className="text-amber-500">any</span> race
+          win <span className="text-indigo-500">any</span> race
         </h1>
         <div className="w-[300px] h-16 relative mx-auto flex justify-center items-center">
           <div className=" left-0 top-[2px]  bg-indigo-200 rounded-full" />
